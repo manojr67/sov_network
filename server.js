4308fd40aadd66ec288ff1bfad9ab7ce353259e4
Manoj Rawat <mj.rawat67@gmail.com>
2025-12-28 14:46:42 +0530

    indexserver

--- server.js
+++ server.js
@@ -6,16 +6,22 @@ const port = process.env.PORT || 10000;
 
 app.use(express.static(__dirname)); 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+aapp.get('/index', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/citadel', (req, res) => res.sendFile(path.join(__dirname, 'citadel.html')));
 app.get('/explorer', (req, res) => res.sendFile(path.join(__dirname, 'explorer.html')));
 
-const server = app.listen(port, () => {
-    console.log(`🚀 Master Relay Online at Port ${port}`);
+app.get('/:page', (req, res) => {
+    const page = req.params.page.toLowerCase();
+    const filePath = path.join(__dirname, `${page}.html`);
+    if (fs.existsSync(filePath)) {
+        res.sendFile(filePath);
+    } else {
+        res.status(404).send("Page Not Found in Mesh");
+    }
 });
 
 const gun = Gun({
     web: server,
-    peers: ['https://gun-manhattan.herokuapp.com/gun'],
+    peers: ['https://gun-manhattan.herokuapp.com/gun', 'https://sov-relay.onrender.com/gun'],
     radisk: true
 });
