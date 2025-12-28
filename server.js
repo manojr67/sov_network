c9001a4a3993efa909b0ab952be7db122142ca91
Manoj Rawat <mj.rawat67@gmail.com>
2025-12-28 15:02:02 +0530

    DATA Final

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const Gun = require('gun');
 const path = require('path');
+const helmet = require('helmet');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 10000;
 
-app.use(express.static(__dirname)); 
+app.use(cors());
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(express.static(__dirname));
 
-aapp.get('/index', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+// 🛡️ Explicit Routing to prevent 404
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/citadel', (req, res) => res.sendFile(path.join(__dirname, 'citadel.html')));
 app.get('/explorer', (req, res) => res.sendFile(path.join(__dirname, 'explorer.html')));
 
-app.get('/:page', (req, res) => {
-    const page = req.params.page.toLowerCase();
-    const filePath = path.join(__dirname, `${page}.html`);
-    if (fs.existsSync(filePath)) {
-        res.sendFile(filePath);
-    } else {
-        res.status(404).send("Page Not Found in Mesh");
-    }
+const server = app.listen(port, () => {
+    console.log(`🚀 Master Relay Online at Port ${port}`);
 });
 
+// Gun Mesh Engine
 const gun = Gun({
     web: server,
-    peers: ['https://gun-manhattan.herokuapp.com/gun', 'https://sov-relay.onrender.com/gun'],
+    peers: ['https://gun-manhattan.herokuapp.com/gun'],
     radisk: true
 });
